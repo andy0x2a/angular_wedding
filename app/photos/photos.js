05d24c02c19208515d7046c2f3f402967d98a363
Andy Newman <v-annew@microsoft.com>
2015-12-01 11:02:01 -0800

    Custom Photo Gallery!

--- app/photos/photos.js
+++ app/photos/photos.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.photos', ['ngRoute'])
+angular.module('myApp.photos', ['ngRoute', 'myApp.service'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/photos', {
@@ -9,9 +9,35 @@ angular.module('myApp.photos', ['ngRoute'])
   });
 }])
 
-.controller('photosController', [function() {
+.controller('photosController', ['$scope', '$filter' ,'$window','apiService' ,function($scope, $filter, $window, api) {
+
+$scope.init = function() {
+	$scope.getPhotos();
+};
+
+$scope.getDocHeight = function() {
+   return $($window).height();
+};
+$scope.showImage = function(photo) {
+	$scope.currentPhoto = photo;
+	$scope.isFullscreen = true;
+
+} ;
+
+$scope.hideImage = function() {
+	console.log("hiding image");
+	$scope.isFullscreen = false;
+	$scope.currentPhoto = undefined;
+};
+$scope.getPhotos = function() {
+	$scope.photoTemplates = api.getPhotos();
+	angular.forEach($scope.photoTemplates.photos, function(photo) {
+		photo.thumb = $scope.photoTemplates.path + "thumb/" + photo.name;
+		photo.url = $scope.photoTemplates.path + "" + photo.name;
 
 
+	});
+}
 
-
+$scope.init();
 }]);
--- app/service/service.js
+++ app/service/service.js
@@ -2,229 +2,74 @@
 
 angular.module('myApp.service',[])
 
-// .service('apiServices', [function() {
-
-
-//         return {
-
-//             postGuest: function() {
-//             console.log('posting guest');
-
-//             },
-
-//             putGuest: function() {
-//                 console.log('put guest');
-//             }
-
-//         }
-// }]);
-
 .service('apiService', ['constants', '$http', function(constants, $http) {
 
-        return {
-            getAllFamilies: function() {
-                
+
+      var _getAllFamilies = function() {
             var req = {
                 method: 'GET',
                 url: constants.config.apiBase + '/family/'
-                
-            };
-            return $http(req);
-            },
-            submitGuests: function(requestData) {
-    
+          };
+          return $http(req);
+      };
+
+
+      var _submitGuests =  function(requestData) {    
             var req = {
                 method: 'POST',
                 url: constants.config.apiBase + '/rsvp/updateList',
                 data:requestData
-                
-            };
-            return $http(req);
-            },
 
-            getStuffOld: function() {
-                return {
-                    guests : [
-                        {name: 'Emily Heard',family: 'newman'},
-
-                        {name: 'Andy Newman',family: 'newman'},
-
-                        {name: 'Geny Heard',family: 'JG'},
-                        {name: 'Jason Heard',family: 'JG'},
-                        {name: 'Ryley',family: 'JG'},
-                        {name: 'Pyper',family: 'JG'},
-
-                        {name: 'Matt Wallace',family:'wallace'},
-                        {name: 'Jenn Calabrigo',family:'wallace'},
-
-                        {name: 'Kirk Klassen',family:'klassen'},
-
-                        {name: 'Merrilee Knowlan-Heard', family: 'Knowlan-heard'},
-                        {name: 'David Heard', family: 'Knowlan-heard'},
-
-                        {name: 'Melissa McComb', family: 'McComb'},
-                        {name: 'Iain McComb', family: 'McComb'},
-                        {name: 'James McComb', family: 'McComb'},
-
-                        {name: 'Karen Newman', family: 'NC'},
-                        {name: 'Aaron Coelho', family: 'NC'},
-
-                        {name:'Sharon Newman', family: 'rs'},
-                        {name: 'Reg Newman', family: 'rs'},
-
-                        {name: 'Chloe Ellis', family: 'ellis'},
-                        {name: 'Lee Ellis', family: 'ellis'},
-
-                        {name: 'Haena Cho', family: 'cho'},
-                        {name: 'Richard Fong', family: 'cho'},
-
-                        {name: 'Samantha Lindberg', family: 'lindberg'},
-                        {name: 'Raymond Blackmore', family: 'lindberg'},
-
-                        {name: 'Michelle Richards', family: 'mrich'},
-
-                        {name: 'Alicia Richards', family: 'richards'},
-                        {name: 'Desi McEwan', family: 'richards'},
-
-                        {name: 'Jeff Newman', family: 'laser'},
-                        {name: 'Jeff_Date', family: 'laser'},
-
-                        {name: 'Matthew Elder', family: 'elder'},
-                        {name: 'Sarah Elder', family: 'elder'},
-
-                        {name: 'Catherine Railton', family: 'railton'},
-
-                        {name: 'Craig Pender', family: 'pender'},
-
-                        {name: 'Krista Manley', family: 'manley'},
-                        {name: 'Corbin Pender', family: 'manley'},
-
-                        {name: 'Cecilia Newman', family: 'grandma'},
-                        {name: 'Jennifer Johnston', family: 'grandma'},
-                        {name: 'Colin Johnston', family: 'grandma'},
-
-                        {name: 'Tina Knowlan', family: 'tk'},
-                        {name: 'Brett Anuik', family: 'tk'},
-
-                        {name: 'Rick Knowlan', family: 'rk'},
-                        {name: 'Shan He', family: 'rk'},
-
-                        {name: 'Tammy Knowlan', family: 'tammy'},
-
-                        {name: 'Randy Knowlan', family: 'randy'},
-
-                        {name: 'Rod Knowlan', family: 'rod'},
-
-                        {name: 'Terry Heard', family: 'th'},
-                        {name: 'Gordon Heard', family: 'th'},
-
-                        {name: 'Lynn Obojski', family: 'obojski'},
-                        {name: 'Eugene Obojski', family: 'obojski'},
-
-                        {name: 'Wendy Sinclair-Smith', family: 'ss'},
-                        {name: 'Elizabeth Sinclair-Smith', family: 'SS'},
-
-                        {name: 'Michele Lee', family: 'lee'},
-
-                        {name: 'Erin Cox', family: 'cox'},
-                        {name: 'Eric Larsen', family: 'cox'},
-
-                        {name: 'Carmen Cohoe', family: 'cohoe'},
-
-                        {name: 'Abhi Ramlugun', family: 'abhi'},
-                        {name: 'Alison Esau', family: 'abhi'},
-
-                        {name: 'Scott McCoach', family: 'mccoach'},
-                        {name: 'Meghan Laurel', family: 'mccoach'},
-
-                        {name: 'Jesse Goodall', family: 'preet'},
-                        {name: 'Preet Dutt', family: 'preet'},
-
-                        {name: 'Laurence Newman', family: 'ln'},
-                        {name: 'Agnes Newman', family: 'ln'},
-
-                        {name: 'Mirand Rowley', family: 'rowley'},
-                        {name: 'Dustin Rowley', family: 'rowley'},
-
-                        {name: 'Chris Newman', family: 'cnewman'},
-
-                        {name: 'Peter Newman', family: 'pnewman'},
-                        {name: 'Brittany', family: 'pnewman'},
-
-                        {name: 'Liz Pederson', family: 'pederson'},
-                        {name: 'Jeff Lockwood', family: 'pederson'},
-
-                        {name: 'Amy Cheng', family: 'cheng'},
-
-                        {name: 'Chris Beal', family: 'bender'},
-
-                        {name: 'Chris Bennett', family: 'bennett'},
-
-                        {name: 'Kiel Skriver', family: 'kiel'},
-
-                        {name: 'Doug Butters', family: 'dbutters'},
-                        {name: 'Grace Butters', family: 'dbutters'},
-                        {name: 'Jacob Butters', family: 'dbutters'},
-
-                        {name: 'Greg Butters', family: 'gbutters'},
-
-                        {name: 'James Butters', family: 'jbutters'},
-                        {name: 'Gayle Butters', family: 'jbutters'},
-
-                        {name: "Dave O'Sullivan", family: 'osullivan'},
-                        {name: "Daves wife O'Sullivan", family: 'osullivan'},
-
-                        {name: 'Giancarlo Brancati', family: 'giancarlo'},
-                        {name: 'Erin Brancati', family: 'giancarlo'},
-
-                        {name: 'Harry Coster', family: 'coster'},
-                        {name: 'Melinda Coster', family: 'coster'},
-
-                        {name: 'Janine Wittal', family: 'wittal'},
-
-                        {name: 'Lynell Anderson', family: 'anderson'},
-                        {name: 'Rod Anderson', family: 'anderson'},
-
-                        {name: 'Matt Anderson', family: 'cowdy'},
-                        {name: 'Dan Seeton', family: 'cowdy'},
-
-                        {name: 'Melanie Anderson', family: 'manderson'},
-                        {name: 'Taylor Mappins', family:'manderson'},
-
-                        {name: 'Ryan Wong', family: 'rwong'},
-
-                        {name: 'Mark Newman', family: 'mnew'},
-                        {name: 'Lolly Newman', family: 'mnew'},
-                        {name: 'Jasmine Newman', family: 'mnew'},
-
-                        {name: 'Kayla Burley', family: 'kayla'},
-
-                        {name: 'Lindsey Luckey', family: 'luckey'},
-                        {name: 'Chris Amond', family: 'luckey'},
-
-                        {name: 'Alexis Eapen', family: 'eapen'},
-                        {name:'Tyler Stevenson', family: 'eapen'},
-
-                        {name: "Leah Knowlan", family: 'leah'},
-
-                        {name: 'Matthew McCarthy', family: 'mccarthy'},
-                        {name: 'Isha McCarthy', family: 'mccarthy'},
-
-                        {name: 'Melissa Cook', family: 'cook'},
-                        {name: 'Tyee Cook', family: 'cook'},
-
-                        {name: 'Tony Coelho', family: 'coelho'},
-                        {name: 'Cindy Coelho', family: 'coelho'},
-                        {name: 'Trevor Coelho', family: 'coelho'},
-
-                        {name: 'Taylor Barnard', family: 'tay'},
-
-                        {name: 'Zahoor Samji', family: 'zahoor'},
-                        {name: 'Taslim Samji', family: 'zahoor'}
-
-                    ],
-
-                }
-            }
-        }
-    }]);
+          };
+          return $http(req);
+      };
+
+      var _getPhotos = function() {
+            var galleryLocation = "/img/gallery/";
+
+            var photos = [
+            {name:"1.jpg"},
+            {name:"2.jpg"},
+            {name:"3.jpg"},
+            {name:"4.jpg"},
+            {name:"5.jpg"},
+            {name:"6.jpg"},
+            {name:"7.jpg"},
+            {name:"8.jpg"},
+            {name:"9.jpg"},
+            {name:"10.jpg"},
+            {name:"11.jpg"},
+            {name:"12.jpg"},
+            {name:"Emily and Andy Engagement-1.jpg"},
+            {name:"Emily and Andy Engagement-2.jpg"},
+            {name:"Emily and Andy Engagement-3.jpg"},
+            {name:"Emily and Andy Engagement-4.jpg"},
+            {name:"Emily and Andy Engagement-5.jpg"},
+            {name:"Emily and Andy Engagement-6.jpg"},
+            {name:"Emily and Andy Engagement-7.jpg"},
+            {name:"Emily and Andy Engagement-8.jpg"},
+            {name:"Emily and Andy Engagement-9.jpg"},
+            {name:"Emily and Andy Engagement-10.jpg"},
+            {name:"Emily and Andy Engagement-11.jpg"},
+            {name:"Emily and Andy Engagement-12.jpg"},
+            {name:"Emily and Andy Engagement-13.jpg"},
+            {name:"Emily and Andy Engagement-14.jpg"},
+            {name:"Emily and Andy Engagement-15.jpg"},
+            {name:"Emily and Andy Engagement-16.jpg"},
+            {name:"Emily and Andy Engagement-17.jpg"},
+            {name:"Emily and Andy Engagement-18.jpg"}
+
+            ];
+
+
+            return {path:galleryLocation, photos};
+
+      }
+
+      return {
+
+            getAllFamilies: _getAllFamilies,
+            submitGuets: _submitGuests,
+            getPhotos: _getPhotos
+      }
+}]);
